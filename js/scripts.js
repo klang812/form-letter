f02140346aea9714a6a838d69eff78de0602a936
Kerry Lang <klang812@gmail.com>
2021-01-14 10:13:25 -0800

    fix bug allowing letter to pop up

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     let yourName = $("input#name").val();
-    $("#your-name").text("name");
+    $("#your-name").text(yourName);
     $(".page1").hide();
     $(".letter").show();
     event.preventDefault();
